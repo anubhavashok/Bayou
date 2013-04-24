448bc5993dc77791950e3f96451ca549b909c475
anubhavashok <anubhavashok93@gmail.com>
2013-04-24 00:25:08 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,4 +1,16 @@
 public class Client
 {
-
+  public void add(SongEntry s)
+  {
+    //send song entry to server that it is connected to.
+  }
+  public void delete(SongEntry s)
+  {
+    //send delete request to server that its connected to.
+  }
+  public void edit(SongEntry s, String URL)
+  {
+    s.setURL(URL);
+    add(s);
+  }
 }
