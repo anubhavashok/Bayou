ba793549a997c63b9bbe268c2800aff4726e9140
anubhavashok <anubhavashok93@gmail.com>
2013-04-24 00:29:58 -0500

    Update SongEntry.java

--- SongEntry.java
+++ SongEntry.java
@@ -3,4 +3,20 @@ public class SongEntry
   private String songName;
   private String URL;
   
+  public String getSongName()
+  {
+    return songName;
+  }
+  public String getURL()
+  {
+    return URL;
+  }
+  public void setSongName(String songName)
+  {
+    this.songName = songName;
+  }
+  public void setURL(String URL)
+  {
+    this.URL=URL;
+  }
 }
