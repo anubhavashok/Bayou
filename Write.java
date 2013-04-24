0b5b85088d6a5b0c8e7a6ab07ff8e9fb677d2821
anubhavashok <anubhavashok93@gmail.com>
2013-04-24 00:56:30 -0500

    Update Write.java

--- Write.java
+++ Write.java
@@ -1,4 +1,26 @@
 public class Write
 {
-  
+  private String op;
+  private SongEntry s;
+  Write(SongEntry s, String op)
+  {
+    setOp(op);
+    setSongEntry(s);
+  }
+  public String getOp()
+  {
+    return op;
+  }
+  public SongEntry getSongEntry()
+  {
+    return s;
+  }
+  public void setOp(String op)
+  {
+    this.op=op;
+  }
+  public void setSongEntry(SongEntry s)
+  {
+    this.s=s;
+  }
 }
