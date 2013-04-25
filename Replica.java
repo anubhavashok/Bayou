d3c1d23ef1c57ce5b99dcb7b88a6444180d643ad
anubhavashok <anubhavashok93@gmail.com>
2013-04-25 00:54:49 -0500

    Update Replica.java

--- Replica.java
+++ Replica.java
@@ -3,10 +3,19 @@ public class Replica
   private ArrayList<SongEntry> database;        //current database version
   private ArrayList<SongEntry> stableDatabase;  //back up after garbage collection
   private ArrayList<Write> writeLog;
-  
-  public void receiveWriteFromClient(Write w)
+  private int id;
+  private int acceptTime=0;
+  private ArrayList<Integer> V;                 // initialize this + implement removal of nodes
+
+  public void receiveWriteFromClient()
   {
-    
+    Write w;                                    //new write that was received
+    w.setReplicaId(id);
+    w.setAcceptTime(acceptTime);
+    writeLog.add(w);
+    Integer i = V.get(id);
+    i=new Integer(i.intValue() + 1);            //increment version id of current replica
+    V.set(id,i);
   }
   
 }
