09b13117292a3b6e76b6fef289c41a537ea808f1
anubhavashok <anubhavashok93@gmail.com>
2013-04-27 14:24:22 -0500

    Update Replica.java

--- Replica.java
+++ Replica.java
@@ -1,7 +1,7 @@
 public class Replica
 {
-  private ArrayList<SongEntry> database;        //current database version
-  private ArrayList<SongEntry> stableDatabase;  //back up after garbage collection
+  private set<SongEntry> database;        //current database version
+  private set<SongEntry> stableDatabase;  //back up after garbage collection
   private ArrayList<Write> writeLog;
   private int id;
   private int acceptTime=0;
