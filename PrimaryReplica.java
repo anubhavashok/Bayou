e1f8dc5a1649bafe772811ea80ab0bdf39bf2d41
anubhavashok <anubhavashok93@gmail.com>
2013-04-27 14:06:58 -0500

    Update PrimaryReplica.java

--- PrimaryReplica.java
+++ PrimaryReplica.java
@@ -1,4 +1,33 @@
 public class PrimaryReplica extends Replica
 {
-
+  private int CSNCount=0;
+  
+  public void mergeWithDatabase()
+  {
+    for(Write w: writeLog)
+    {
+      if(w.getOp().equals("add"))
+      {
+        database.add(w.getSongEntry());
+        //increment CSN, ADD CSN TO WRITE IN WRITE LOG
+      }
+      if(w.getOp().equals("delete"))
+      {
+        database.remove(w.getSongEntry());
+        //increment CSN, ADD CSN TO WRITE IN WRITE LOG
+      }
+      if(w.getOp().equals("modify"))
+      {
+        for(SongEntry s : database)
+        {
+          if(s.getSongName().equals(w.getSongEntry().getSongName()))
+          {
+            database.delete(w.getSongEntry());
+          }
+        }
+        database.add(w.getSongEntry());
+        //increment CSN, ADD CSN TO WRITE IN WRITE LOG
+      }
+    }
+  }
 }
