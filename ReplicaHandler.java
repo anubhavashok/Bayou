730ea01dd80fb02767132e0eec27e24c9392da3a
anubhavashok <anubhavashok93@gmail.com>
2013-05-03 01:38:30 -0500

    Update ReplicaHandler.java

--- ReplicaHandler.java
+++ ReplicaHandler.java
@@ -1,4 +1,59 @@
 public class ReplicaHandler
 {
-
+    private Replica r;
+    public void startAntiEntropy()
+    {
+      
+    }
+    public Instruction waitForInstructions() throws IOException
+    {
+          ServerSocket ss= new ServerSocket(2000);
+      		//System.out.println(n);
+      		ss.setReuseAddress(true); 
+      		ss.setSoTimeout(0);
+      		//n++;
+      	try
+       	 {
+        	 	Socket s = ss.accept();
+        		s.setReuseAddress(true); 
+        		s.setSoTimeout(0);
+    		
+        		//System.out.println(s.getSoTimeout() );
+        		//System.out.println(s.getReuseAddress() );
+      	   	InputStream is = s.getInputStream();
+      	   	ObjectInputStream ois = new ObjectInputStream(is);
+      	  	Instruction I=null;
+      
+      	  	I=(Instruction)ois.readObject();
+      	   	is.close();
+      	   	s.close();
+      	   	ss.close();  		
+             return I;
+      	}
+      
+      	catch(Exception e)
+      	{
+      		System.out.println(e.getLocalizedMessage());
+      		return null;
+      	}
+    }
+    public void executeInstruction(Instruction I)
+    {
+      if(I.getInst().equals("startAntiEntropy"))
+      {
+        r.setServerConnection(I.getReplicaIP());
+        if(I.getOrder()==0)
+        {
+          r.selectToSend();
+          r.receiveWriteFromReplica();
+        }
+        else
+        {
+          r.receiveWriteFromReplica();
+          r.selectToSend();
+        }
+      }
+    }
+      
+    }
 }
