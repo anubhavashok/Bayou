4eb0d8b2575f51c98c34df301a32978188211385
anubhavashok <anubhavashok93@gmail.com>
2013-05-03 01:43:43 -0500

    Update Instruction.java

--- Instruction.java
+++ Instruction.java
@@ -1,4 +1,32 @@
 public class Instruction
 {
-
+    private String instr;
+    private String replicaIP;
+    private int order;
+    
+    public void setInstr(String instr)
+    {
+      this.instr=instr;
+    }
+    public void setReplicaIP(String rip)
+    {
+      this.replicaIP=rip;
+    }
+    public void setOrder(int o)
+    {
+      this.order=o;
+    }
+    public String getInstr()
+    {
+      return instr;
+    }
+    public String getReplicaIP()
+    {
+      return replicaIP;
+    }
+    public int getOrder()
+    {
+      return order;
+    }
+    
 }
